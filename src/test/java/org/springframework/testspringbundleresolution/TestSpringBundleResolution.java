453b1eec3956fded8a417bd7af4cf264d346198d
Florian Waibel <fwaibel@eclipsesource.com>
2015-07-04 08:50:30 +0200

    Reworks test to use local Spring Framework 4.2.0.RC2 bundles generated with Eclipse Bundle Recipies.

--- src/test/java/org/springframework/testspringbundleresolution/TestSpringBundleResolution.java
+++ src/test/java/org/springframework/testspringbundleresolution/TestSpringBundleResolution.java
@@ -21,22 +21,27 @@ import static org.ops4j.pax.exam.CoreOptions.provision;
 
 import javax.inject.Inject;
 
-import org.ops4j.pax.exam.junit.PaxExam;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
 @RunWith(PaxExam.class)
 public class TestSpringBundleResolution {
 
-    private static final String SPRINGFRAMEWORK_RELEASE_MAVEN_URL = "http://repository.springsource.com/maven/bundles/release";
+    // private static final String SPRINGFRAMEWORK_MAVEN_URL =
+    // "http://repository.springsource.com/maven/bundles/release";
+    private static final String SPRINGFRAMEWORK_MAVEN_URL = "file:" + System.getProperty("user.home") + "/.m2/repository/";
+
+    private static final String SPRINGFRAMEWORK_GROUP_ID = "MY_EBR_BUNDLES_GROUP";
 
     private static final String EBR_EXTERNAL_MAVEN_URL = "http://repository.springsource.com/maven/bundles/external";
 
-    private static final String SPRING_VERSION = "3.2.4.RELEASE";
+    // private static final String SPRINGFRAMEWORK_VERSION = "3.2.4.RELEASE";
+    private static final String SPRINGFRAMEWORK_VERSION = "4.2.0-SNAPSHOT";
 
     @Inject
     private BundleContext bundleContext;
@@ -44,38 +49,40 @@ public class TestSpringBundleResolution {
     @Configuration
     public static Option[] configuration() throws Exception {
         return options(//
-            provisionSpringBundle("org.springframework.core"), //
-            provisionSpringBundle("org.springframework.beans"), //
             provisionSpringBundle("org.springframework.aop"), //
             provisionSpringBundle("org.springframework.aspects"), //
+            provisionSpringBundle("org.springframework.beans"), //
             provisionSpringBundle("org.springframework.context"), //
             provisionSpringBundle("org.springframework.context.support"), //
+            provisionSpringBundle("org.springframework.core"), //
             provisionSpringBundle("org.springframework.expression"), //
             provisionSpringBundle("org.springframework.jdbc"), //
             provisionSpringBundle("org.springframework.jms"), //
+            provisionSpringBundle("org.springframework.messaging"), //
             provisionSpringBundle("org.springframework.orm"), //
             provisionSpringBundle("org.springframework.oxm"), //
             provisionSpringBundle("org.springframework.transaction"), //
             provisionSpringBundle("org.springframework.web"), //
-            provisionSpringBundle("org.springframework.web.servlet"), //
-            provisionSpringBundle("org.springframework.web.portlet"), //
-            
-            // mandatory dependencies common to multiple Spring bundles
+            provisionSpringBundle("org.springframework.webmvc"), //
+            provisionSpringBundle("org.springframework.webmvc.portlet"), //
+            provisionSpringBundle("org.springframework.websocket"), //
+
+        // mandatory dependencies common to multiple Spring bundles
             provisionExternalBundle("org.apache.commons", "com.springsource.org.apache.commons.logging", "1.1.1"), //
-            
-            // spring-aop mandatory dependencies
+
+        // spring-aop mandatory dependencies
             provisionExternalBundle("org.aopalliance", "com.springsource.org.aopalliance", "1.0.0"), //
 
-            // spring-jms mandatory dependencies
+        // spring-jms mandatory dependencies
             provisionExternalBundle("javax.jms", "com.springsource.javax.jms", "1.1.0"), //
 
-            // spring-web mandatory dependencies
+        // spring-web mandatory dependencies
             provisionExternalBundle("javax.servlet", "javax.servlet", "3.0.0.v201103241009"), //
 
-            // spring-webmvc-portlet mandatory dependencies
+        // spring-webmvc-portlet mandatory dependencies
             provisionExternalBundle("javax.portlet", "com.springsource.javax.portlet", "2.0.0.v20110525"), //
 
-            junitBundles());
+        junitBundles());
     }
 
     private static Option provisionExternalBundle(String groupId, String artifactId, String version) {
@@ -86,24 +93,21 @@ public class TestSpringBundleResolution {
         // Use Pax URL. See http://team.ops4j.org/wiki/display/paxurl/Mvn+Protocol
         return "mvn:" + repositoryUrl + "!" + groupId + "/" + artifactId + "/" + version + "/" + "jar";
     }
-    
+
     private static Option provisionSpringBundle(String artifactId) {
         return provision(bundle(springBundleUrl(artifactId)));
     }
 
     private static String springBundleUrl(String artifactId) {
-        String version = SPRING_VERSION;
-        String groupId = "org.springframework";
-        String repositoryUrl = SPRINGFRAMEWORK_RELEASE_MAVEN_URL;
-        return plainMavenUrl(repositoryUrl, groupId, artifactId, version);
+        return plainMavenUrl(SPRINGFRAMEWORK_MAVEN_URL, SPRINGFRAMEWORK_GROUP_ID, artifactId, SPRINGFRAMEWORK_VERSION);
     }
-    
+
     private static String plainMavenUrl(String repositoryUrl, String groupId, String artifactId, String version) {
         return repositoryUrl + "/" + groupId.replaceAll("\\.", "/") + "/" + artifactId + "/" + version + "/" + artifactId + "-" + version + ".jar";
     }
 
     @Test
-    public void testSpringCoreIsResolvable() throws Exception {
+    public void springBundlesShouldBeActive() throws Exception {
         int found = 0;
         Bundle[] bundles = this.bundleContext.getBundles();
         for (Bundle bundle : bundles) {
@@ -113,6 +117,6 @@ public class TestSpringBundleResolution {
                 assertEquals(symbolicName + " is not ACTIVE", Bundle.ACTIVE, bundle.getState());
             }
         }
-        assertEquals("Unexpected number of Spring bundles found", 15, found);
+        assertEquals("Unexpected number of Spring bundles found", 17, found);
     }
 }
